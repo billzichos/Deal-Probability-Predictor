5e0415aa39ae129079118b9e51514e9d34a102a0
billzichos <billzichos@hotmail.com>
2015-05-30 07:23:09 -0400

    continuing to enhance the iCalendar functionality
    I have confirmed the output file loads to my Outlook.com calendar.

--- iCalendar.R
+++ iCalendar.R
@@ -1,23 +1,24 @@
 # script accepts parameters and creates an ics text file for calendar imports.
 
-
-
-function(startDate, subject) {
+function(startDate, subject, description) {
         
-        line1 <- 'BEGIN:VCALENDAR'
-        line2 <- 'VERSION:2.0'
-        line3 <- 'BEGIN:VEVENT'
-        line4 <- paste('SUMMARY:', subject, sep='')
-        line5 <- 'END:VEVENT'
-        line6 <- 'END:VCALENDAR'
+        line01 <- 'BEGIN:VCALENDAR'
+        line02 <- 'VERSION:2.0'
+        line03 <- 'PRODID:R'
+        line04 <- 'BEGIN:VEVENT'
+        line05 <- 'UID:bill.zichos'
+        line06 <- 'DTSTAMP:20150530T170000Z'
+        line07 <- 'ORGANIZER;CN=Bill Zichos:MAILTO:billzichos@hotmail.com'
+        line08 <- 'DTSTART:20150530T170000Z'
+        line09 <- 'DTEND:20150530T180000Z'
+        line10 <- paste('SUMMARY:', subject, sep='')
+        line11 <- paste('DESCRIPTION:', description, sep='')
+        line12 <- 'END:VEVENT'
+        line13 <- 'END:VCALENDAR'
         
         f <- file("test.ics")
-        writeLines(c(line1, line2, line3, line4, line5, line6), f)
+        writeLines(
+                c(line01, line02, line03, line04, line05, line06, line07, line08, line09, line10, line11, line12, line13),
+                f)
         close(f)
 }
-        
-        
-        f <- file("test.ics")
-        writeLines(c('line 1','line 2 hello world'), f)
-        close(f)
-        
