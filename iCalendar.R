f0695290dc6e2231dae7b297735b6b8977238760
billzichos <billzichos@hotmail.com>
2015-05-30 07:31:59 -0400

    iCalendar reorg

--- iCalendar.R
+++ iCalendar.R
@@ -2,23 +2,22 @@
 
 function(startDate, subject, description) {
         
-        line01 <- 'BEGIN:VCALENDAR'
-        line02 <- 'VERSION:2.0'
-        line03 <- 'PRODID:R'
-        line04 <- 'BEGIN:VEVENT'
-        line05 <- 'UID:bill.zichos'
-        line06 <- 'DTSTAMP:20150530T170000Z'
-        line07 <- 'ORGANIZER;CN=Bill Zichos:MAILTO:billzichos@hotmail.com'
-        line08 <- 'DTSTART:20150530T170000Z'
-        line09 <- 'DTEND:20150530T180000Z'
-        line10 <- paste('SUMMARY:', subject, sep='')
-        line11 <- paste('DESCRIPTION:', description, sep='')
-        line12 <- 'END:VEVENT'
-        line13 <- 'END:VCALENDAR'
-        
         f <- file("test.ics")
-        writeLines(
-                c(line01, line02, line03, line04, line05, line06, line07, line08, line09, line10, line11, line12, line13),
-                f)
+        
+        writeLines(c(line01 <- 'BEGIN:VCALENDAR',
+                     line02 <- 'VERSION:2.0',
+                     line03 <- 'PRODID:R',
+                     line04 <- 'BEGIN:VEVENT',
+                     line05 <- 'UID:bill.zichos',
+                     line06 <- 'DTSTAMP:20150530T170000Z',
+                     line07 <- 'ORGANIZER;CN=Bill Zichos:MAILTO:billzichos@hotmail.com',
+                     line08 <- 'DTSTART:20150530T170000Z',
+                     line09 <- 'DTEND:20150530T180000Z',
+                     line10 <- paste('SUMMARY:', subject, sep=''),
+                     line11 <- paste('DESCRIPTION:', description, sep=''),
+                     line12 <- 'END:VEVENT',
+                     line13 <- 'END:VCALENDAR'
+                     ), f)
+                
         close(f)
 }
