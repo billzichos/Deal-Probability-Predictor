3fcf8de8333695347074bd94cdc11bb838e2d8e5
billzichos <billzichos@hotmail.com>
2015-07-09 23:15:16 -0400

    add comments

--- updateResults.R
+++ updateResults.R
@@ -1,26 +1,35 @@
 #Update the periodic snapshot with Results if known
 library(dplyr)
 
-snapshotFilename <- "OpenDeals20150601.csv"
+# setwd("~/Private-Github-Files")
 
-results <- read.csv("~//Private-Github-Files//sample_results.csv")
+# specify the snapshot file to update with actual results
+snapshotFilename <- "OpenDeals20150508.csv"
+
+# import the actual results
+results <- read.csv("~//Private-Github-Files//results.csv")
 # str(results)
 
+# import the point-in-time snapshot
 snapshot <- read.csv(paste("~//Private-Github-Files//", snapshotFilename, sep =""))
 # str(snapshot)
 # summary(snapshot)
 
+# merge the snapshot and results files, creating a single dataset
 comb <- merge(snapshot, results, by.x = 'Deal.Number', by.y = 'Deal.Number')
 # summary(comb)
 # str(comb)
 
+# update the closedsaswon field based on the value in the status field.
 comb$ClosedAsWon[comb$Status=="Lost"] <- 0
 comb$ClosedAsWon[comb$Status=="Won"] <- 1
 
+# add only the fields from the snapshot dataset to the final dataset.
 completeDataset <- select(comb, 1:21)
 # str(completeDataset)
 # str(completeDataset$ClosedAsWon)
 # summary(completeDataset)
 # summary(completeDataset$ClosedAsWon)
 
+# overwrite the orginal snapshot file
 write.csv(completeDataset,paste("~//Private-Github-Files//",snapshotFilename, sep = ""), row.names = FALSE)
